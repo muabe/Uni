da6d0f6d19055851a6877db76e733f40953a23ab
MarkJ <markjmind@gmail.com>
2016-11-28 00:25:01 +0900

    EditTextUtil 추가

---
+++ uni/src/main/java/com/markjmind/uni/util/EditTextUtils.java
@@ -0,0 +1,31 @@
+package com.markjmind.uni.util;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+/**
+ * Created by MarkJ on 2016-11-28.
+ */
+
+public class EditTextUtils {
+    private static void hideKeyboard(View view){
+        if ( view != null ) {
+            InputMethodManager imm = (InputMethodManager) view.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    public static void hideKeyboard(Activity activity){
+        hideKeyboard(activity.getCurrentFocus());
+
+    }
+
+    public static void hideKeyboard(Dialog dialog){
+        hideKeyboard(dialog.getCurrentFocus());
+    }
+
+
+}
